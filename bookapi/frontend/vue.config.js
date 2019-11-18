506f2315bbb41acf41335f06813d2a5e9b571f53
Chaehwa <r.chaehwa@gmail.com>
2019-11-18 14:12:39 +0900

    port change back

--- bookapi/frontend/vue.config.js
+++ bookapi/frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001/api',
+        target: 'http://localhost:3000/api',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
