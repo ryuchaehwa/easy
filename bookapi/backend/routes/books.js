a4586227764800ef48e4e03c52f72a41934e4b12
Chaehwa <r.chaehwa@gmail.com>
2019-11-18 15:20:18 +0900

    mysql login id changed

--- bookapi/backend/routes/books.js
+++ bookapi/backend/routes/books.js
@@ -5,8 +5,8 @@ var mysql = require('mysql')
 const db = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: 'root',
-    password: '111111',
+    user: 'chaehwa',
+    password: 'password',
     database: 'book'
 })
 
